ff2e91a2d1568b792b52bb09239ed795f59d143b
JESUS EDUARDO YATACO PALACIOS <jesuseduardoyatacopalacios339@gmail.com>
2025-06-02 11:42:26 -0500

    Corrigiendo consultas

--- query.sql
+++ query.sql
@@ -1,27 +1,19 @@
 -- CONSULTAS A LAS PREGUNTAS
 -- 1:  ¿Cuántos alumnos desaprobados tenemos en total?
 SELECT COUNT(DISTINCT usuario_id) AS alumnos_desaprobados
-FROM desarrollos
+FROM  desarrollos 
 WHERE puntaje_total < 12;
 
 
 -- 2:¿Cuántos alumnos aprobados en un determinado curso tenemos?
 SELECT COUNT(*) AS alumnos_aprobados
 FROM desarrollos
-WHERE evaluacion_id = 1
-  AND puntaje_total >= 12;
+WHERE evaluacion_id = 1 
+AND puntaje_total >= 12;
 
 
 -- 3:¿A cuántos exámenes está inscrito un alumno y cuántos de ellos están resueltos y pendientes?
-SELECT
-  a.usuario_id,
-  COUNT(DISTINCT a.evaluacion_id) AS total_asignados,
-  COUNT(DISTINCT d.evaluacion_id) AS total_resueltos,
-  (COUNT(DISTINCT a.evaluacion_id) - COUNT(DISTINCT d.evaluacion_id)) AS total_pendientes
-FROM asignaciones a
-LEFT JOIN desarrollos d ON a.usuario_id = d.usuario_id AND a.evaluacion_id = d.evaluacion_id
-WHERE a.usuario_id = 1
-GROUP BY a.usuario_id;
+
 
 -- 4: ¿Cuál es la mejor y peor calificación de una determinada evaluación?
 SELECT 
